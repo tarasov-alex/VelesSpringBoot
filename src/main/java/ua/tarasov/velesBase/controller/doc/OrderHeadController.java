a8efa895245521d246a27dda74f742d0fa11aaff
tarasov-alex <54375480+tarasov-alex@users.noreply.github.com>
2022-08-08 09:46:42 +0300

    2022_08_08_09_46

--- src/main/java/ua/tarasov/velesBase/controller/doc/OrderHeadController.java
+++ src/main/java/ua/tarasov/velesBase/controller/doc/OrderHeadController.java
@@ -29,14 +29,13 @@ public class OrderHeadController {
     private static final String MSG_ERROR = "ERROR";
 
 
-
     @PostMapping("/add")
     public ResponseEntity addAll(@RequestBody Iterable<OrderHead> obs, @RequestHeader("token") String token) {
         try {
             if (!tokenService.checkToken(token)) return ResponseEntity.badRequest().body("BAD TOKEN");
             return ResponseEntity.ok(orderHeadService.addAll(obs));
         } catch (Exception e) {
-            errorService.add(new Error(getClass().getSimpleName()+"/addAll", e.getMessage(), new Date()));
+            errorService.add(new Error(getClass().getSimpleName() + "/addAll", e.getMessage(), new Date()));
             return ResponseEntity.badRequest().body(MSG_ERROR);
         }
     }
@@ -46,9 +45,10 @@ public class OrderHeadController {
         try {
             if (!tokenService.checkToken(token)) return ResponseEntity.badRequest().body("BAD TOKEN");
             StringBuilder text = new StringBuilder("INSERT INTO orders ( date, find_name, guid, number, selling, send, kontr_guid, trade_guid, comment, summa, first_form, discount, summa_pay ) VALUES");
-            String comma = "";
-            for (OrderHead obj : orderHeadService.giveAllByIdAgent(idAgent)) {
-                text.append(comma)
+            final String[] comma = {""};
+            orderHeadService.giveAllByIdAgent(idAgent).forEach(obj -> {
+
+                text.append(comma[0])
                         .append(" ('").append(obj.getDate().getTime())
                         .append("', '").append(obj.getFindName())
                         .append("', '").append(obj.getIdDoc())
@@ -63,33 +63,53 @@ public class OrderHeadController {
                         .append(", ").append(obj.getDiscount())
                         .append(", ").append(obj.getSumPay())
                         .append(")");
-                comma = ",";
-            }
+                comma[0] = ",";
+
+            });
+
+//            for (OrderHead obj : orderHeadService.giveAllByIdAgent(idAgent)) {
+//                text.append(comma[0])
+//                        .append(" ('").append(obj.getDate().getTime())
+//                        .append("', '").append(obj.getFindName())
+//                        .append("', '").append(obj.getIdDoc())
+//                        .append("', '").append(obj.getNumber())
+//                        .append("', '").append(obj.getNumberPay())
+//                        .append("', ").append(obj.getSendToBase())
+//                        .append(", '").append(obj.getIdClient())
+//                        .append("', '").append(obj.getIdShop())
+//                        .append("', '").append(obj.getComment())
+//                        .append("', ").append(obj.getSum())
+//                        .append(", ").append(obj.getNonCash())
+//                        .append(", ").append(obj.getDiscount())
+//                        .append(", ").append(obj.getSumPay())
+//                        .append(")");
+//                comma[0] = ",";
+//            }
             return ResponseEntity.ok(text);
         } catch (Exception e) {
-            errorService.add(new Error(getClass().getSimpleName()+"/giveAll", e.getMessage(), new Date()));
+            errorService.add(new Error(getClass().getSimpleName() + "/giveAll", e.getMessage(), new Date()));
             return ResponseEntity.badRequest().body(MSG_ERROR);
         }
     }
 
     @GetMapping("/clear")
-    public ResponseEntity clear(@RequestHeader("token") String token){
-        try{
+    public ResponseEntity clear(@RequestHeader("token") String token) {
+        try {
             if (!tokenService.checkToken(token)) return ResponseEntity.badRequest().body("BAD TOKEN");
             return ResponseEntity.ok(orderHeadService.clear());
-        }catch (Exception e){
-            errorService.add(new Error(getClass().getSimpleName()+"/clear", e.getMessage(), new Date()));
+        } catch (Exception e) {
+            errorService.add(new Error(getClass().getSimpleName() + "/clear", e.getMessage(), new Date()));
             return ResponseEntity.badRequest().body(MSG_ERROR);
         }
     }
 
     @PostMapping("/getListUpdate")
-    public ResponseEntity giveNotUpdate(@RequestBody Iterable<UpdateObj> obs, @RequestHeader("token") String token){
-        try{
+    public ResponseEntity giveNotUpdate(@RequestBody Iterable<UpdateObj> obs, @RequestHeader("token") String token) {
+        try {
             if (!tokenService.checkToken(token)) return ResponseEntity.badRequest().body("BAD TOKEN");
             return ResponseEntity.ok(orderHeadService.giveListNotUpdateDoc(obs));
-        }catch (Exception e){
-            errorService.add(new Error(getClass().getSimpleName()+"/clear", e.getMessage(), new Date()));
+        } catch (Exception e) {
+            errorService.add(new Error(getClass().getSimpleName() + "/clear", e.getMessage(), new Date()));
             return ResponseEntity.badRequest().body(MSG_ERROR);
         }
     }
